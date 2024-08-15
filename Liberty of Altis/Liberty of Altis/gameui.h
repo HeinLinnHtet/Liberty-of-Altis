be8d91b5af5483b65df750805acced0a7f70fccf
AlexPastGrey <140066150+AlexPastGrey@users.noreply.github.com>
2024-08-15 11:16:03 +0800

    Update gameui.h

--- Liberty of Altis/Liberty of Altis/gameui.h
+++ Liberty of Altis/Liberty of Altis/gameui.h
@@ -10,11 +10,13 @@ private:
     int HP, attack, range, cost, size;
 public:
     gameui();
+    int unitcount = 0;
     void cutscene1();
     void stats();
     int choice;
     void gamestart(gameui& player);
     int getstats(int H, int a, int r, int c, int s);
+    void getstats(std::string nameset,  int H, int a, int r, int c, int s);
     void turtorial();
     void gotoxy(int x, int y);
     void printdelay(const std::string& text, int delay);
