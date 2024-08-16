49b9e200e2226b0fd8c4f6d7a3059b07aa1214b3
speedy81523 <85233801+speedy81523@users.noreply.github.com>
2024-08-16 11:15:29 +0800

    updated attack

--- Liberty of Altis/Liberty of Altis/AlliedTroops.cpp
+++ Liberty of Altis/Liberty of Altis/AlliedTroops.cpp
@@ -31,7 +31,79 @@ void AlliedTroops::Movement(char direction)
 
 bool AlliedTroops::canattack(Entity& other, Entity& second, environmentalObjs& obj)
 {
+    //a temp variable to store ture/false
+    bool attackable = false;
 
-	return false;
+    //check if any enemy entity(allied troops) within the range spaces of the entity(enemy troop)
+    for (int i = 0; i < Range; i++) {
+        if (PosXY.GetY() + Range - i == other.PosXY.GetY()) {
+            //in front
+            //check if theres any objects in the way
+            if (PosXY.GetY() + Range - i == second.PosXY.GetY()) {
+                //check for fellow enemies
+                attackable == false;
+            }
+            else if (PosXY.GetY() + Range - i == obj.objPos.GetY()) {
+                //check for any objects (trees etc.)
+                attackable == false;
+            }
+            else {
+                //if none then shoot
+                attackable == true;
+            }
+        }
+        else if (PosXY.GetY() - Range + i == other.PosXY.GetY()) {
+            //behind
+            //check if theres any objects in the way
+            if (PosXY.GetY() - Range + i == second.PosXY.GetY()) {
+                //check for fellow enemies
+                attackable == false;
+            }
+            else if (PosXY.GetY() - Range + i == obj.objPos.GetY()) {
+                //check for any objects (trees etc.)
+                attackable == false;
+            }
+            else {
+                //if none then shoot
+                attackable == true;
+            }
+        }
+        else if (PosXY.GetX() + Range - i == other.PosXY.GetX()) {
+            //to the right
+            //check if theres any objects in the way
+            if (PosXY.GetX() + Range - i == second.PosXY.GetX()) {
+                //check for fellow enemies
+                attackable == false;
+            }
+            else if (PosXY.GetX() + Range - i == obj.objPos.GetX()) {
+                //check for any objects (trees etc.)
+                attackable == false;
+            }
+            else {
+                //if none then shoot
+                attackable == true;
+            }
+        }
+        else if (PosXY.GetX() - Range + i == other.PosXY.GetX()) {
+            //to the left
+            //check if theres any objects in the way
+            if (PosXY.GetX() - Range + i == second.PosXY.GetX()) {
+                //check for fellow enemies
+                attackable == false;
+            }
+            else if (PosXY.GetX() - Range + i == obj.objPos.GetX()) {
+                //check for any objects (trees etc.)
+                attackable == false;
+            }
+            else {
+                //if none then shoot
+                attackable == true;
+            }
+        }
+    }
+
+    //return the thing
+    return attackable;
 }
+
 
