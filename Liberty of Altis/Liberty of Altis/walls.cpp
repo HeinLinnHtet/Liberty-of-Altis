5fbbe10518da86e2e539a7ff3b99b666c9bf53df
bluebear223123 <ozheng22@gmail.com>
2024-08-20 10:27:46 +0800

    walls update

--- Liberty of Altis/Liberty of Altis/walls.cpp
+++ Liberty of Altis/Liberty of Altis/walls.cpp
@@ -4,3 +4,13 @@
 walls::walls(int h, char sym, int xp, int yp) : health(h), environmentalObjs(sym, xp, yp)
 {
 }
+
+void walls::sethealth(int a)
+{
+	health = a;
+}
+
+int walls::gethealth()
+{
+	return health;
+}
