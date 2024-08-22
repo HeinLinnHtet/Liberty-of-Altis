ddd1f2cd322aa72c936feab0afe2587ccc02eee6
bluebear223123 <ozheng22@gmail.com>
2024-08-22 15:31:39 +0800

    ...

--- Liberty of Altis/Liberty of Altis/main.cpp
+++ Liberty of Altis/Liberty of Altis/main.cpp
@@ -24,51 +24,53 @@ int main(void) {
 	mapmain4 testlvl4;
 	mapmain5 testlvl5;
 
-	//cutscene.cutscene1();
-	//system("CLS");
+	cutscene.cutscene1();
+	system("CLS");
 
-	//testlvl1.map1game();
-	//system("CLS");
+	testlvl1.map1game();
+	system("CLS");
 
-	//cutscene.cutscene2();
-	//system("CLS");
-	//testlvl2.map2game();
-	//system("CLS");
+	cutscene.cutscene2();
+	system("CLS");
+	testlvl2.map2game();
+	system("CLS");
 
-	//cutscene.cutscene3();
-	//system("CLS");
+	cutscene.cutscene3();
+	system("CLS");
 
-	//testshop.shopping();
+	testshop.shopping();
 
-	////These are the values of what you bought from the shop
-	//
-	//maininventory.riflemansetter(testshop.inventory.riflemangetter());
-	//maininventory.artillerysetter(testshop.inventory.artillerygetter());
-	//maininventory.grenadiersetter(testshop.inventory.grenadiergetter());
-	//maininventory.IFVsetter(testshop.inventory.IFVgetter());
-	//maininventory.machinegunnersetter(testshop.inventory.machinegunnergetter());
-	//maininventory.motarsetter(testshop.inventory.motargetter());
-	//maininventory.tanksetter(testshop.inventory.tankgetter());
+	//These are the values of what you bought from the shop
+	
+	maininventory.riflemansetter(testshop.inventory.riflemangetter());
+	maininventory.artillerysetter(testshop.inventory.artillerygetter());
+	maininventory.grenadiersetter(testshop.inventory.grenadiergetter());
+	maininventory.IFVsetter(testshop.inventory.IFVgetter());
+	maininventory.machinegunnersetter(testshop.inventory.machinegunnergetter());
+	maininventory.motarsetter(testshop.inventory.motargetter());
+	maininventory.tanksetter(testshop.inventory.tankgetter());
 
-	////Level 3
-	//while (testlvl3.GetGameOver() == false) {
-	//	maininventory.backpak();
-	//	system("CLS");
-	//	testlvl3.map3game(
-	//		maininventory.Troop_broughtin[0],
-	//		maininventory.Troop_broughtin[1],
-	//		maininventory.Troop_broughtin[2],
-	//		maininventory.Troop_broughtin[3],
-	//		maininventory.Troop_broughtin[4],
-	//		maininventory.Troop_broughtin[5],
-	//		maininventory.Troop_broughtin[6]);
-	//}
+	//Level 3
+	while (testlvl3.GetGameOver() == false) {
+		maininventory.backpak();
+		system("CLS");
+		testlvl3.map3game(
+			maininventory.Troop_broughtin[0],
+			maininventory.Troop_broughtin[1],
+			maininventory.Troop_broughtin[2],
+			maininventory.Troop_broughtin[3],
+			maininventory.Troop_broughtin[4],
+			maininventory.Troop_broughtin[5],
+			maininventory.Troop_broughtin[6]);
+	}
 
-	////pay day
-	//testshop.cashearned(500);
+	system("CLS");
 
-	//cutscene.cutscene4();
-	//system("CLS");
+	//pay day
+	testshop.cashearned(500);
+
+	cutscene.cutscene4();
+	system("CLS");
 
 	testshop.shopping();
 
@@ -96,6 +98,8 @@ int main(void) {
 			maininventory.Troop_broughtin[5],
 			maininventory.Troop_broughtin[6]);
 	}
+
+	system("CLS");
 
 	//pay day 2
 	testshop.cashearned(1000);
@@ -128,6 +132,8 @@ int main(void) {
 			maininventory.Troop_broughtin[5],
 			maininventory.Troop_broughtin[6]);
 	}
+
+	system("CLS");
 
 	cutscene.cutscence6();
 	system("CLS");
